96da32ae9a39846cc5ca903a8917ed5e72a7250b
Josephine <josephinemary200802@gmail.com>
2025-05-15 15:48:43 +0530

    middeware

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 require('./db/index')
+const doctor = require('./control/doctor')
 const app = express();
-
+app.use('/', doctor)
 app.use(express.json());
 
 app.listen(6000, () => {
