cc9f63f86b9c61f0e11f9a809c7ff441a35d504a
Josephine <josephinemary200802@gmail.com>
2025-05-15 10:49:18 +0530

    dp

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const dp = require('./db/index')
+require('./db/index')
 const app = express();
 
 app.use(express.json());
-app.use('/', dp)
+
 app.listen(6000, () => {
     console.log('server connection')
 })
